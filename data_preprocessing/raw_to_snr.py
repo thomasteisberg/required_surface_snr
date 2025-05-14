Fix rssnr calculation

data_preprocessing/raw_to_snr.py
@@ -1,7 +1,7 @@
 import os
 import pandas as pd
 import numpy as np
-from snrfinder import snrfinder
+from snrfinder import calculate_rssnr
 from scipy.io import loadmat
 import argparse
 
@@ -55,14 +55,13 @@ if __name__ == "__main__":
             print(f'Now reading {csvPath} and {matPath}')
 
             try:
-                snrs = snrfinder(csvPath, matPath, ice_sheet=dataset)
-                df = pd.DataFrame(snrs, columns=['x', 'y', 'snr'])
+                df = calculate_rssnr(csvPath, matPath, ice_sheet=dataset)
                 df['source_csv_file'] = os.path.basename(csvPath)
                 df['source_mat_file'] = os.path.basename(matPath)
                 df['source_dir'] = os.path.basename(top_level_dir)
                 snr_dfs_list.append(df)
                 stats[top_level_dir]['success'] += 1
-            except Exception as e:
+            except ValueError as e:
                 print(f'Error processing {csvPath} and {matPath}: {e}')
                 print(e)
                 stats[top_level_dir]['other_failure'] += 1

data_preprocessing/snrfinder.py
@@ -4,6 +4,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 import pyproj
 import h5py
+import scipy.constants
 
 epsg_3031 = pyproj.Proj("EPSG:3031")
 epsg_3413 = pyproj.Proj("EPSG:3413")
@@ -18,7 +19,9 @@ def ll2ps(lat, lon, proj_ps=epsg_3031):
     x, y = proj_ps(lon, lat)
     return x, y
 
-def snrfinder(csv, matfile, ice_sheet='antarctica'):
+def calculate_rssnr(csv_path, mat_path, ice_sheet='antarctica', save_plot=True, plot_path=None):
+    e_ice = 3.15
+    vel_ice = scipy.constants.c / np.sqrt(e_ice)
 
     if ice_sheet == 'greenland':
         proj_ps = epsg_3413
@@ -27,89 +30,141 @@ def snrfinder(csv, matfile, ice_sheet='antarctica'):
     else:
         raise ValueError("Invalid ice sheet specified. Use 'greenland' or 'antarctica'.")
 
+    # Load and sanity check data
     sci = False
     hdf = False
+
     #Load .mat file
-    try: #attempt to open with scipy
-        mat = loadmat(matfile)
-        Data = mat.get('Data')
-        Data = np.array(Data)
-        Time = mat.get('Time')
-        Time = np.array(Time)
-        Surface = mat.get('Surface')
-        Surface = np.array(Surface)
+    try: # attempt to open with scipy
+        mat = loadmat(mat_path)
+        mat["Data"] = mat["Data"].T
         sci = True
     except NotImplementedError: #use hdf reader if scipy doesn't work
-        mat = h5py.File(matfile, 'r')
-        Data = mat.get('Data')
-        Data = np.array(Data)
-        Time = mat.get('Time')
-        Time = np.array(Time)
-        Surface = mat.get('Surface')
-        Surface = np.array(Surface)
+        mat = h5py.File(mat_path, 'r')
         hdf = True
 
     # Load .csv file
-    csvdata = pd.read_csv(csv)
-    surf = csvdata['SURFACE']
-    bott = csvdata['BOTTOM']
-    elev = csvdata['ELEVATION']
-    thicc = csvdata['THICK']
-    lon = csvdata['LON']
-    lat = csvdata['LAT']
-    e_ice = 3.15
+    csvdata = pd.read_csv(csv_path, na_values=[-9999, "-9999"])
 
-    surf_real = elev - surf
-    bott_real = elev - bott
-    v = 299792458 / np.sqrt(e_ice)
-    propdelayS = surf_real / v
-    propdelayB = bott_real / v
+    csv_time_rel = np.array(csvdata['UTCTIMESOD'] - csvdata['UTCTIMESOD'].iloc[0])
+    mat_time_rel = np.squeeze(mat['GPS_time'] - mat['GPS_time'][0][0])
 
-    lon_list = []
-    lat_list = []
-    snr_list = []
+    # Sanity checks on time
+    if np.any(np.diff(csv_time_rel) < 0):
+        print("CSV time is not monotonic")
+        raise ValueError("CSV time is not monotonic")
+    if np.any(np.diff(mat_time_rel) < 0):
+        print("MAT time is not monotonic")
+        raise ValueError("MAT time is not monotonic")
+    if np.abs(csv_time_rel[-1] - mat_time_rel[-1]) > 3:
+        print(f"CSV and MAT timespans differ by more than 3 seconds. Max diff: {np.max(np.abs(csv_time_rel - mat_time_rel))} seconds")
+        raise ValueError("CSV and MAT timespans differ by more than 3 seconds")
 
-    for i in range(len(csvdata)):
-        # Check if line has any missing data
-        if (surf[i] == -9999 or bott[i] == -9999 or elev[i] == -9999 or thicc[i] == -9999):
-            continue
+    decimation = np.ceil(len(csv_time_rel) / len(mat_time_rel)).astype(int)
+    #print(f"Decimation factor: {decimation}")
 
-        # Find geometric spreading correction
-        geom_spreadingB = (surf[i] + (thicc[i] / np.sqrt(e_ice))) ** 2
-        geom_spreadingS = (surf[i]) ** 2
+    csvdata = csvdata[::decimation].dropna()
 
-        # Find surface data
-        fasttimeS = np.argmin(np.abs(Time - propdelayS[i]))  # Finds surface fasttime
-        slowtime = np.argmin(np.abs(Surface - propdelayS[i]))  # Finds slowtime
+    # Extract surface and bed power information
 
-        # Find bed data
-        fasttimeB = np.argmin(np.abs(Time - propdelayB[i]))  # Finds surface fasttime
+    mat_slow_idx = []
+    mat_surf_idx = []
+    mat_bott_idx = []
+    surface_pwr_db = []
+    bottom_pwr_db = []
+    rssnr_db = []
 
-        if (hdf): # FLIP THE INDEXES INTO THE DATA MATRIX
-            surfwatts = np.abs(Data[slowtime, fasttimeS]) * geom_spreadingS  # Pwr w/ geometric spreading correction
-            bedwatts = np.abs(Data[slowtime, fasttimeB]) * geom_spreadingB
-        else: # SAME AS MATLAB INDEXING
-            surfwatts = np.abs(Data[fasttimeS, slowtime]) * geom_spreadingS
-            bedwatts = np.abs(Data[fasttimeB, slowtime]) * geom_spreadingB
+    fasttime_mat = np.squeeze(mat['Time'])
 
-        # Find SNR using surface and bed power difference
-        snr = 10 * np.log10(surfwatts / bedwatts)
+    def pick_power(slowtime_idx, fasttime_idx, fasttime_half_width_idx=2):
+        start_idx = np.maximum(0, fasttime_idx - fasttime_half_width_idx)
+        end_idx = np.minimum(len(fasttime_mat)-1, fasttime_idx + fasttime_half_width_idx)
+        return np.max(mat['Data'][slowtime_idx, start_idx:end_idx])
 
-        # Add to list of data to plot
-        lon_list.append(lon[i])
-        lat_list.append(lat[i])
-        snr_list.append(snr)
+    # Loop through each row in the CSV file
+    for i, row in csvdata.iterrows():
+        slowtime_idx = np.argmin(np.abs(csv_time_rel[i] - mat_time_rel))
+        
+        surface_fasttime = row['SURFACE'] / (scipy.constants.c/2)
+        bottom_fasttime = surface_fasttime + (row['THICK'] / (vel_ice/2))
 
-    x, y = ll2ps(lat_list, lon_list, proj_ps=proj_ps)  # Convert lat/lon to polar stereographic coordinates
-    """ plt.scatter(x, y, 20, snr_list, cmap='viridis')
-    cb = plt.colorbar()
-    cb.set_label('snr')
-    plt.show() """
+        surf_idx = np.argmin(np.abs(surface_fasttime - mat['Time']))
+        bot_idx = np.argmin(np.abs(bottom_fasttime - mat['Time']))
+        
 
-    values = np.column_stack((x, y, snr_list))  # Returns ps coordinates + snr for future use
-    return values
+        # Find surface and bed power
+        surf_pwr = pick_power(slowtime_idx, surf_idx)
+        bot_pwr = pick_power(slowtime_idx, bot_idx)
 
-#snrfinder(r'cresis_data\2023_Antarctica_BaslerMKB_\csv_20240107_01_\Data_20240107_01_028.csv', r'cresis_data\2023_Antarctica_BaslerMKB_\CSARP_qlook_20240107_01_\Data_20240107_01_028.mat')
-#snrfinder('Data_20240107_01_028.csv', 'Data_20240107_01_028.mat')
-#snrfinder(r'cresis_data\2018_Antarctica_DC8_\csv_20181010_01_\Data_20181010_01_001.csv', r'cresis_data\2018_Antarctica_DC8_\CSARP_qlook_20181010_01_\Data_20181010_01_001.mat') # uses hdf reader, flips data matrix from MatLab version
-#snrfinder(r'cresis_data\2023_Antarctica_BaslerMKB_\csv_20240104_01_\Data_20240104_01_029.csv', r'cresis_data\2023_Antarctica_BaslerMKB_\CSARP_qlook_20240104_01_\Data_20240104_01_029.mat') # uses scipy reader, matrix dimensions are the same as in Matlab
+        # Calculate geometric spreading corrections
+        geom_spreading_surf = row['SURFACE']**2
+        geom_spreading_bed = (row['SURFACE'] + (row['THICK'] / np.sqrt(e_ice)))**2
+
+        rssnr_lin = surf_pwr * geom_spreading_surf / (bot_pwr * geom_spreading_bed)
+
+        # Add results to lists
+        mat_slow_idx.append(slowtime_idx)
+        mat_surf_idx.append(surf_idx)
+        mat_bott_idx.append(bot_idx)
+        surface_pwr_db.append(10 * np.log10(surf_pwr))
+        bottom_pwr_db.append(10 * np.log10(bot_pwr))
+        rssnr_db.append(10 * np.log10(rssnr_lin))
+
+    df_res = pd.DataFrame({
+        'mat_slow_idx': mat_slow_idx,
+        'mat_surf_idx': mat_surf_idx,
+        'mat_bott_idx': mat_bott_idx,
+        'surface_pwr_db': surface_pwr_db,
+        'bottom_pwr_db': bottom_pwr_db,
+        'snr': rssnr_db,
+        'surface': csvdata['SURFACE'],
+        'thickness': csvdata['THICK'],
+        'bottom': csvdata['BOTTOM'],
+        'latitude': csvdata['LAT'],
+        'longitude': csvdata['LON'],
+        })
+
+    # Convert lat/lon to polar stereographic coordinates
+    x, y = ll2ps(df_res['latitude'], df_res['longitude'], proj_ps=proj_ps)
+    df_res['x'] = x
+    df_res['y'] = y
+
+    # Optionally, produce a debugging plot
+    if save_plot:
+        fig, (ax, ax_pwr, ax_rssnr) = plt.subplots(3, 1, figsize=(12, 9), sharex=True, gridspec_kw={'height_ratios': [2, 1, 1]})
+
+        # Plot radargram
+        X, Y = np.meshgrid(np.arange(len(np.squeeze(mat["GPS_time"]))), np.arange(len(np.squeeze(mat["Time"]))))
+        ax.pcolormesh(X, Y, 10*np.log10(np.abs(mat["Data"])).T, cmap='gray', shading='nearest')
+        ax.set_ylim(1.2*np.max(df_res['mat_bott_idx']), 0)
+        ax.set_ylabel('Fast Time Index')
+
+        # Plot picks on radargram
+        ax.plot(df_res['mat_slow_idx'], df_res['mat_surf_idx'], '--', markersize=0.1, label='Surface Picks')
+        ax.plot(df_res['mat_slow_idx'], df_res['mat_bott_idx'], '--', markersize=0.1, label='Bottom Picks')
+        ax.legend()
+
+        # Plot surface and bed power
+        ax_pwr.plot(df_res['mat_slow_idx'], df_res['surface_pwr_db'], label='Surface Power')
+        ax_pwr.plot(df_res['mat_slow_idx'], df_res['bottom_pwr_db'], label='Bed Power')
+        ax_pwr.set_ylabel('Power [dB]')
+        ax_pwr.legend()
+        ax_pwr.grid()
+
+        # Plot RSSNR
+        ax_rssnr.plot(df_res['mat_slow_idx'], df_res['snr'], 'k-', label='RSSNR')
+        ax_rssnr.plot(df_res['mat_slow_idx'], df_res['surface_pwr_db'] - df_res['bottom_pwr_db'], c='gray', linestyle=":", label='Surface - Bed')
+        ax_rssnr.set_ylabel('Power [dB]')
+        ax_rssnr.set_xlabel('Slow Time Index')
+        ax_rssnr.legend()
+        ax_rssnr.grid()
+
+        ax.set_title(f"{mat_path}")
+        
+        if plot_path is None:
+            plot_path = f"{csv_path}.png"
+        print(f"Saving plot to {plot_path}")
+        fig.savefig(plot_path)
+        plt.close(fig)
+    
+    return df_res