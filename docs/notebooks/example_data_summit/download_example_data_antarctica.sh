e361d76b3a2b38824f76c4d68b064e9f000499c3
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2025-07-17 08:28:29 -0400

    Chose new tracks in a smaller region for better mapping

--- docs/notebooks/example_data_summit/download_example_data_antarctica.sh
+++ docs/notebooks/example_data_summit/download_example_data_antarctica.sh
@@ -4,15 +4,22 @@ mkdir -p antarctica
 
 pushd antarctica
 
+wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/CSARP_qlook/20141115_05/Data_20141115_05_004.mat
+wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/csv_good/20141115_05/Data_20141115_05_004_160511.csv
+
+wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/CSARP_qlook/20141115_06/Data_20141115_06_010.mat
+wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/csv_good/20141115_06/Data_20141115_06_010_170655.csv
+
+wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/CSARP_qlook/20141115_06/Data_20141115_06_011.mat
+wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/csv_good/20141115_06/Data_20141115_06_011_171320.csv
+
+wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/CSARP_qlook/20141115_06/Data_20141115_06_012.mat
+wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/csv_good/20141115_06/Data_20141115_06_012_171915.csv
+
 wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/CSARP_qlook/20141115_06/Data_20141115_06_013.mat
 wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/csv_good/20141115_06/Data_20141115_06_013_172521.csv
 
-wget -nc https://data.cresis.ku.edu/data/rds/2018_Antarctica_DC8/CSARP_qlook/20181112_04/Data_20181112_04_025.mat
-wget -nc https://data.cresis.ku.edu/data/rds/2018_Antarctica_DC8/csv_good/20181112_04/Data_20181112_04_025.csv
+wget -nc https://data.cresis.ku.edu/data/rds/2018_Antarctica_DC8/CSARP_qlook/20181112_01/Data_20181112_01_026.mat
+wget -nc https://data.cresis.ku.edu/data/rds/2018_Antarctica_DC8/csv_good/20181112_01/Data_20181112_01_026.csv
 
-wget -nc https://data.cresis.ku.edu/data/rds/2016_Antarctica_DC8/CSARP_qlook/20161115_03/Data_20161115_03_039.mat
-wget -nc https://data.cresis.ku.edu/data/rds/2016_Antarctica_DC8/csv_good/20161115_03/Data_20161115_03_039_174840.csv
-
-wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/CSARP_qlook/20141026_07/Data_20141026_07_016.mat
-wget -nc https://data.cresis.ku.edu/data/rds/2014_Antarctica_DC8/csv_good/20141026_07/Data_20141026_07_016_200939.csv
 popd
