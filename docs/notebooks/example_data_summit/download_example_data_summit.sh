360391ae0ef1449f8cfa3c39b70475460a128e6d
Thatcher Chamberlin <j.thatcher.c@gmail.com>
2025-07-17 07:26:03 -0400

    Create `summit` folder for Summit data

--- docs/notebooks/example_data_summit/download_example_data_summit.sh
+++ docs/notebooks/example_data_summit/download_example_data_summit.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
+mkdir -p summit
+
+pushd summit
+
 wget -nc https://data.cresis.ku.edu/data/rds/2019_Greenland_P3/CSARP_qlook/20190512_01/Data_20190512_01_057.mat
 wget -nc https://data.cresis.ku.edu/data/rds/2019_Greenland_P3/csv_good/20190512_01/Data_20190512_01_057.csv
 
@@ -14,3 +18,5 @@ wget -nc https://data.cresis.ku.edu/data/rds/2018_Greenland_P3/csv_good/20180423
 
 wget -nc https://data.cresis.ku.edu/data/rds/2018_Greenland_P3/CSARP_qlook/20180423_01/Data_20180423_01_053.mat
 wget -nc https://data.cresis.ku.edu/data/rds/2018_Greenland_P3/csv_good/20180423_01/Data_20180423_01_053.csv
+
+popd
